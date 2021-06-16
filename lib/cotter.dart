b61cde3c86296a819cc3a72a5cccd7b10214eee0
Joshua Suatan <joshuacarl19@gmail.com>
2021-06-17 01:24:09 +0700

    fix: export the exceptions handler

--- lib/cotter.dart
+++ lib/cotter.dart
@@ -1,5 +1,7 @@
 library cotter;
 
+export 'src/exceptions/token.dart';
+export 'src/exceptions/user.dart';
 export 'src/cotter.dart';
 export 'src/models/user.dart';
 export 'src/models/event.dart';
